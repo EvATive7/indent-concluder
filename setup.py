d57f83ae8787e0482b32afe1d4088610cb935e15
EvATive7 <evative7@gmail.com>
2024-05-10 20:46:22 +0800

    chore: ver

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="indent-concluder",
-    version="1.1.1",
+    version="1.1.2",
     author="EvATive7",
     author_email="",
     long_description=long_description,
